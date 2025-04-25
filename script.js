0788fc73cee48b7946aa6af0df762492d6763f4d
Julian <reffkejul@gmail.com>
2025-04-25 07:42:07 +0200

    nur ein paar Zahlen

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 alert("Welcome to Jubilant Spoon!");
 alert("We are glad to have you here!");
 alert("Please enjoy your stay and let us know if you need any assistance.");
+alert("Have a great day ahead!");
+alert("Thank you for visiting Jubilant Spoon!");
